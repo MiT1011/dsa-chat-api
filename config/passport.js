cdea8714e4c7c589d6358f456366203f71ca304a
MiT1011 <meetnpatel101112@gmail.com>
2024-10-02 11:58:38 +0530

    passport url changed

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,7 @@ dotenv.config();
 const gStrategy = new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/redirect',
+    callbackURL: 'https://dsa-chat-api.onrender.com/auth/google/redirect',
     // agent: agent
 }, async (accessToken, refreshToken, profile, done) => {
     try {
